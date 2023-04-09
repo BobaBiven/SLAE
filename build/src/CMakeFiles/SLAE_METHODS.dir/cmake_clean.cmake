331bbb5a0f7c604721aca9801cac8cb3de4ef9cb
Askar <agabitov25@gmail.com>
2023-04-09 09:31:19 +0300

    some changes

---
+++ build/src/CMakeFiles/SLAE_METHODS.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SLAE_METHODS.dir/slae_methods/CSR_matrix.cpp.o"
+  "CMakeFiles/SLAE_METHODS.dir/slae_methods/CSR_matrix.cpp.o.d"
+  "CMakeFiles/SLAE_METHODS.dir/slae_methods/ConjugateGradient.cpp.o"
+  "CMakeFiles/SLAE_METHODS.dir/slae_methods/ConjugateGradient.cpp.o.d"
+  "CMakeFiles/SLAE_METHODS.dir/slae_methods/FixedPointIteration.cpp.o"
+  "CMakeFiles/SLAE_METHODS.dir/slae_methods/FixedPointIteration.cpp.o.d"
+  "CMakeFiles/SLAE_METHODS.dir/slae_methods/GaussSeidelMethod.cpp.o"
+  "CMakeFiles/SLAE_METHODS.dir/slae_methods/GaussSeidelMethod.cpp.o.d"
+  "CMakeFiles/SLAE_METHODS.dir/slae_methods/GradientDescent.cpp.o"
+  "CMakeFiles/SLAE_METHODS.dir/slae_methods/GradientDescent.cpp.o.d"
+  "CMakeFiles/SLAE_METHODS.dir/slae_methods/JacobiMethod.cpp.o"
+  "CMakeFiles/SLAE_METHODS.dir/slae_methods/JacobiMethod.cpp.o.d"
+  "CMakeFiles/SLAE_METHODS.dir/slae_methods/TridiagonalMatrix.cpp.o"
+  "CMakeFiles/SLAE_METHODS.dir/slae_methods/TridiagonalMatrix.cpp.o.d"
+  "CMakeFiles/SLAE_METHODS.dir/slae_methods/TridiagonalMatrixAlgorithm.cpp.o"
+  "CMakeFiles/SLAE_METHODS.dir/slae_methods/TridiagonalMatrixAlgorithm.cpp.o.d"
+  "CMakeFiles/SLAE_METHODS.dir/slae_methods/tools.cpp.o"
+  "CMakeFiles/SLAE_METHODS.dir/slae_methods/tools.cpp.o.d"
+  "libSLAE_METHODS.pdb"
+  "libSLAE_METHODS.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SLAE_METHODS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/SLAE_METHODS.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SLAE_METHODS.
+# This may be replaced when dependencies are built.
